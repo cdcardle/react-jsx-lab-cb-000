ee534fe45a6c947a819a1ccd2d9be7e20173efd8
Chris Cardle <cdcardle@gmail.com>
2019-02-04 19:58:26 +0000

    Automatically backed up by Learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,11 +1,13 @@
-// class RegistrationForm extends React.Component {
-//   render() {
-//     return (
-//       <form>
-//         <input type="text">
-//         <input type="password">
-//         <button type="submit"></button>
-//       </form>
-//     )
-//   }
-// }
+class registrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text">
+        <input type="password">
+        <button type="submit"></button>
+      </form>
+    )
+  }
+}
+
+export default registrationForm
